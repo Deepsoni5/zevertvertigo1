dedfae775c60e684d48107cc2fcb4d17ec13f617
Deepsoni5 <fichadiyadeep@gmail.com>
2025-01-07 18:51:19 +0530

    chnage in do donts sup

--- app/components/DosAndDonts.jsx
+++ app/components/DosAndDonts.jsx
@@ -32,7 +32,7 @@ const DosAndDonts = () => {
         >
           Donts
         </button>
-        <sup className="font-bold text-lg">1-9,36-40</sup>
+        <sup className="font-bold text-lg hidden md:flex">1-9,36-40</sup>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full">
         {activeTab === "dos" && (
